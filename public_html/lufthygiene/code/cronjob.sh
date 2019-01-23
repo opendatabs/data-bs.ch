4cc4c89a2a7176e826176ea2658ddc58cfc84f5a
Jonas Bieri <jonas.bieri@gmail.com>
2019-01-23 16:02:46 +0000

    Correct path

--- public_html/lufthygiene/code/cronjob.sh
+++ public_html/lufthygiene/code/cronjob.sh
@@ -3,7 +3,7 @@
 #/home/opendata/
 #/home/ubuntu/workspace/
 
-export basedir="/home/ubuntu/workspace/public_html/lufthygiene/daten/roh"
+export basedir="/home/ubuntu/workspace/public_html/lufthygiene/roh"
 #Append contents of each partial csv file (except the two header lines) to the complete csv file, then archive the partial files
 tail -q -n+3 $basedir/airmet_bs_chrischona_roh.csv >> $basedir/airmet_bs_chrischona_2010_bis_aktuell.csv 
 #Remove 1 header line, sort by YYYY MM DDD HH:MM:SS, remove duplicates, save to tempfile (for details on sorting, see see https://www.unix.com/unix-for-dummies-questions-and-answers/131166-sort-field-date-mm-dd-yyyy.html)
