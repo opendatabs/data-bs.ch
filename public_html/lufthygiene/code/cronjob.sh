3034e302931026992b0c9fb4c244bb33f6260d77
Jonas Bieri <jonas.bieri@gmail.com>
2019-03-21 17:30:52 +0000

    Beautify

--- public_html/lufthygiene/code/cronjob.sh
+++ public_html/lufthygiene/code/cronjob.sh
@@ -4,7 +4,7 @@
 #/home/ubuntu/workspace/
 
 export basedir="/home/ubuntu/workspace/public_html/lufthygiene/roh"
-#Append contents of each partial csv file (except the two header lines) to the complete csv file, then archive the partial files
+#Append contents of the partial csv file (except the two header lines) to the complete csv file
 tail -q -n+3 $basedir/airmet_bs_chrischona_roh.csv >> $basedir/airmet_bs_chrischona_2010_bis_aktuell.csv 
 #Remove 1 header line, sort by YYYY MM DDD HH:MM:SS, remove duplicates, save to tempfile (for details on sorting, see see https://www.unix.com/unix-for-dummies-questions-and-answers/131166-sort-field-date-mm-dd-yyyy.html)
 tail -q -n+2 $basedir/airmet_bs_chrischona_2010_bis_aktuell.csv | sort -u -t";" -k1.7,1.10 -k1.4,1.5 -k1.1,1.2 -k1.12,1.19 -o $basedir/chrischona_sort_temp.csv
@@ -15,5 +15,7 @@ cat $basedir/airmet_bs_chrischona_header.csv $basedir/chrischona_sort_temp.csv >
 rm $basedir/chrischona_sort_temp.csv
 rm $basedir/airmet_bs_chrischona_header.csv
 
-#everything on one line
-#export basedir="/home/ubuntu/workspace/public_html/lufthygiene/daten/roh" && tail -q -n+3 $basedir/airmet_bs_chrischona_roh.csv >> $basedir/airmet_bs_chrischona_2010_bis_aktuell.csv  && tail -q -n+2 $basedir/airmet_bs_chrischona_2010_bis_aktuell.csv | sort -u -t";" -k1.7,1.10 -k1.4,1.5 -k1.1,1.2 -k1.12,1.19 -o $basedir/chrischona_sort_temp.csv && head -n1  $basedir/airmet_bs_chrischona_2010_bis_aktuell.csv >  $basedir/airmet_bs_chrischona_header.csv && cat $basedir/airmet_bs_chrischona_header.csv $basedir/chrischona_sort_temp.csv > $basedir/airmet_bs_chrischona_2010_bis_aktuell.csv && rm $basedir/chrischona_sort_temp.csv && rm $basedir/airmet_bs_chrischona_header.csv
+#everything on one line for c9.io
+#export basedir="/home/ubuntu/workspace/public_html/lufthygiene/roh" && tail -q -n+3 $basedir/airmet_bs_chrischona_roh.csv >> $basedir/airmet_bs_chrischona_2010_bis_aktuell.csv && tail -q -n+2 $basedir/airmet_bs_chrischona_2010_bis_aktuell.csv | sort -u -t";" -k1.7,1.10 -k1.4,1.5 -k1.1,1.2 -k1.12,1.19 -o $basedir/chrischona_sort_temp.csv && head -n1  $basedir/airmet_bs_chrischona_2010_bis_aktuell.csv >  $basedir/airmet_bs_chrischona_header.csv && cat $basedir/airmet_bs_chrischona_header.csv $basedir/chrischona_sort_temp.csv > $basedir/airmet_bs_chrischona_2010_bis_aktuell.csv && rm $basedir/chrischona_sort_temp.csv && rm $basedir/airmet_bs_chrischona_header.csv
+#everything on one line for cyon
+#export basedir="/home/opendata/public_html/lufthygiene/roh" && tail -q -n+3 $basedir/airmet_bs_chrischona_roh.csv >> $basedir/airmet_bs_chrischona_2010_bis_aktuell.csv && tail -q -n+2 $basedir/airmet_bs_chrischona_2010_bis_aktuell.csv | sort -u -t";" -k1.7,1.10 -k1.4,1.5 -k1.1,1.2 -k1.12,1.19 -o $basedir/chrischona_sort_temp.csv && head -n1  $basedir/airmet_bs_chrischona_2010_bis_aktuell.csv >  $basedir/airmet_bs_chrischona_header.csv && cat $basedir/airmet_bs_chrischona_header.csv $basedir/chrischona_sort_temp.csv > $basedir/airmet_bs_chrischona_2010_bis_aktuell.csv && rm $basedir/chrischona_sort_temp.csv && rm $basedir/airmet_bs_chrischona_header.csv
