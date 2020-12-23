36ab7c58431c323a02197f5c192250d2d3a4adef
jb3-2 <jonas.bieri@gmail.com>
2020-12-23 09:20:12 +0100

    Remove daily archive of hosp dataset

--- cronjobs/stata/cronjobs.sh
+++ cronjobs/stata/cronjobs.sh
@@ -1,4 +1,6 @@
 # Backup the current data files with a timestamp in its name so that old values are retained (because the file contains a rolling window of values)
 # Escape % because crontab sees this as line breaks, see https://stackoverflow.com/a/38487305/5005585
 # 20 0 * * *
-cp /home/opendata/public_html/stata/md/covid19_cases/covid_hosp.csv /home/opendata/public_html/stata/md/covid19_cases/archiv/covid_hosp_$(date "+\%Y-\%m-\%dT\%H-\%M-\%S").csv
+
+# Not required anymore.
+# cp /home/opendata/public_html/stata/md/covid19_cases/covid_hosp.csv /home/opendata/public_html/stata/md/covid19_cases/archiv/covid_hosp_$(date "+\%Y-\%m-\%dT\%H-\%M-\%S").csv
