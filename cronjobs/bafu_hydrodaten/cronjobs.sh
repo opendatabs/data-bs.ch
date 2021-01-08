0b0bd9fd17da39e765f2e5b3add47ff4367c486c
jb3-2 <jonas.bieri@gmail.com>
2021-01-08 15:36:01 +0100

    Move file to archive folder

--- cronjobs/bafu_hydrodaten/cronjobs.sh
+++ cronjobs/bafu_hydrodaten/cronjobs.sh
@@ -1,2 +1,8 @@
 # */5 * * * *
-/home/opendata/scripts/hydrodata.ch__organize_by_date.sh
+# /home/opendata/scripts/hydrodata.ch__organize_by_date.sh
+
+# 57 23 * * *
+# There's one data file for today that is being updated every 5 minutes or so. Before midnight, move this file to
+# the archive. This way, ODS only parses the new file and keeps the unchanched old files untouched in its cache,
+# so that updating the dataset fast.
+mv /home/opendata/public_html/stata/hydrodata.ch/hydropro/2289/realtime/* /home/opendata/public_html/stata/hydrodata.ch/hydropro/2289/archive/
